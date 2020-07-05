964941c21ca1c9129711fdfa7c8eeb0415eb7a93
xuewei111 <m13358109269_1@163.com>
2020-07-05 17:43:31 +0800

    整合

---
+++ blibli-spring-boot-02-config/src/main/java/com/example/bliblispringboot02config/BlibliSpringBoot02ConfigApplication.java
@@ -0,0 +1,13 @@
+package com.example.bliblispringboot02config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlibliSpringBoot02ConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlibliSpringBoot02ConfigApplication.class, args);
+    }
+
+}
---
+++ blibli-spring-boot-02-config/src/test/java/com/example/bliblispringboot02config/BlibliSpringBoot02ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bliblispringboot02config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlibliSpringBoot02ConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
